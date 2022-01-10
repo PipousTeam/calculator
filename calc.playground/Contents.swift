ab053f86e76578f1c614192a4f6c715b8c7ede88
Soomin Lee <soomin@videolabs.io>
2022-01-10 14:39:25 +0100

    Initial commit

---
+++ calc.playground/Contents.swift
@@ -0,0 +1,12 @@
+/* Write a Calculator class with some basic functionalities
+ *  - Addition
+ *  - Subtraction
+ *  - Division
+ *  - Multiplication
+ * Each of these functions take two Int parameter, one named numberLeft and one named numberRight.
+ * They return the result of the operation.
+**/
+
+// Code Here
+
+
